3caefd4853dc074ac274c92dfd2aa4440cc65468
minagishl <mvxd568.co@icloud.com>
2024-01-02 23:42:29 +0900

    Update layout and tailwind.config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
 import './globals.css';
+import { Inter as FontSans } from 'next/font/google';
+
+const fontSans = FontSans({
+	subsets: ['latin'],
+	variable: '--font-sans',
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
 			<head />
 			<body>
-				<div className='min-w-screen p-2'>{children}</div>
+				<div className='min-w-screen min-h-screen p-2'>{children}</div>
 			</body>
 		</html>
 	);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config: Config = {
 	content: [
@@ -11,6 +12,21 @@ const config: Config = {
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
 				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			minHeight: {
+				screen: '100dvh',
+			},
+			height: {
+				screen: '100dvh',
+			},
+			minWidth: {
+				screen: '100dvw',
+			},
+			width: {
+				screen: '100dvw',
+			},
+			fontFamily: {
+				sans: ['var(--font-sans)', ...fontFamily.sans],
 			},
 		},
 	},
