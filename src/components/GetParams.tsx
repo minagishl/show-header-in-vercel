0a5e48a7e89347154df77ada342a232211fefb3d
minagishl <mvxd568.co@icloud.com>
2024-01-03 00:08:41 +0900

    spelling mistake

--- src/components/GetParams.tsx
+++ src/components/GetParams.tsx
@@ -4,14 +4,14 @@ import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 export default function GetParams() {
-	const [redirect_uri, setRedirect_url] = useState<string | null>(null);
+	const [redirect_url, setRedirect_url] = useState<string | null>(null);
 	const params = useSearchParams();
 
 	useEffect(() => {
 		// Query string
-		setRedirect_url(params.get('redirect_uri'));
+		setRedirect_url(params.get('redirect_url'));
 	}, [params]);
 
 	// Takes a long time to load the first time.
-	return <span>Redirect to URL: {redirect_uri ? redirect_uri : 'Unknown'}</span>;
+	return <span>Redirect to URL: {redirect_url ? redirect_url : 'Unknown'}</span>;
 }
